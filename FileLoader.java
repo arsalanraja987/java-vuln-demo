a248d5e6221509da5cd540394cf1d3cef9cfb08c
arsalanraja987 <arsalanthepathanman@gmail.com>
2025-07-03 10:59:03 -0400

    Update FileLoader.java

--- FileLoader.java
+++ FileLoader.java
@@ -1,11 +1,1 @@
-import java.io.File;
 
-public class FileLoader {
-    public static void main(String[] args) {
-        String fileName = "../../etc/passwd"; // Simulating attacker input
-        File file = new File("/var/data/" + fileName); // 🚨 VULNERABLE CODE
-        System.out.println("Loading file: " + file.getPath());
-    }
-}
- 
- 
