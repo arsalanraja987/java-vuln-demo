83d610b38480120ba571ebdabfe4d04a7eea9292
arsalanraja987 <arsalanthepathanman@gmail.com>
2025-07-03 10:55:22 -0400

    Moved FileLoader.java into src folder

---
+++ src/FileLoader.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class FileLoader {
+    public static void main(String[] args) throws IOException {
+        String input = args[0];
+        File file = new File(input); // 🛑 Vulnerable: path traversal
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        System.out.println(reader.readLine());
+        reader.close();
+    }
+}
